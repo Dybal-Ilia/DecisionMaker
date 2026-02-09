a61ed4ae97fa7e1b4d1ad412d9dea81c6e164f68
Dybal-Ilia <dybal.156@gmail.com>
2026-02-09 14:40:14 +0100

    feat: update WorkerState and ChatState schemas to simplify instructions and corrections types

--- src/graph/schemas.py
+++ src/graph/schemas.py
@@ -1,5 +1,4 @@
 from typing import TypedDict, Annotated
-from pydantic import BaseModel, Field
 from langchain_core.messages import AIMessage
 import operator
 
@@ -7,11 +6,10 @@ import operator
 class WorkerState(TypedDict):
     question: str
     name: str
-    instructions: list[str]
+    instructions: str
     messages: Annotated[list, operator.add]
-    corrections: Annotated[list, operator.add] 
-    score: float
     counter: int
+    corrections: str
 
 class ChatState(TypedDict):
     question: str
@@ -20,12 +18,5 @@ class ChatState(TypedDict):
     aggregated_messages: Annotated[list, operator.add]
     final_response: AIMessage
 
-class DecomposerResponse(BaseModel):
-    instructions: list[str]
-
-
-class ReflectionResponse(BaseModel):
-    score: float = Field(default=0, description="A message score against provided question")
-    correction: str = Field(..., description="Instructions for corrections")
 
 
