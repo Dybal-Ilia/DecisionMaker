38ad53069285f1c811bc50f4f30d02dbad21e2af
Dybal-Ilia <dybal.156@gmail.com>
2026-02-09 14:39:37 +0100

    feat: enhance utils module with logging and presets loading functions

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,27 @@
 from langchain_core.prompts import ChatPromptTemplate
+import logging
 import yaml
-
 
 def load_prompt(name):
     with open(file="src/prompts.yaml", mode="r", encoding="utf-8") as f:
         all_prompts = yaml.safe_load(f)
         prompt_text = all_prompts[name]
     return ChatPromptTemplate.from_template(prompt_text)
+
+def get_logger():
+    logging.basicConfig(
+        format="{asctime}-{levelname}-{name}-{message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M"
+        )
+    logger = logging.getLogger(__name__)
+    return logger
+
+def load_presets():
+    with open(file="src/presets.yaml", mode="r", encoding="utf-8") as f:
+        presets = yaml.safe_load(f)
+        return presets
+
+def load_team(preset_name:str):
+    presets = load_presets()
+    return presets[preset_name] 
