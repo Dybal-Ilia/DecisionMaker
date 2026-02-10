20d6401e86028f39bfac44a20059f3965afe869c
Dybal-Ilia <dybal.156@gmail.com>
2026-02-10 14:20:30 +0100

    refactor: wrap logic into OOP style

--- src/graph/persona.py
+++ src/graph/persona.py
@@ -20,97 +20,103 @@ def web_search(query: str):
     """A tool designed for information search on the Internet
     
     Args:
-        query: a query for a web-search to be executed   
+        query (str): a query for a web-search to be executed   
     """
 
     _tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
+    logger.info("Web-search tool is being executed")
     return _tavily_client.search(query=query, search_depth="basic", max_results=3)
 
 
-llm = ChatGroq(model="llama-3.3-70b-versatile", max_tokens=5096)
 tools = [web_search]
-llm_with_tools = llm.bind_tools(tools)
-tool_node = ToolNode(tools)
 
 
+class Worker:
+    def __init__(self, model_name):
+        self.llm = ChatGroq(model=model_name, api_key=GROQ_API_KEY)
+        self.graph = self._build_persona_graph()
 
-def persona_call(state:WorkerState):
-    name = state["name"]
-    prompt = load_prompt(name)
-    chain = prompt | llm_with_tools
-    question = state["question"]    
-    messages = state["messages"]
-    corrections = state["corrections"]
-    instructions = state["instructions"]
-    response = chain.invoke({
-        "question": question,
-        "messages": messages,
-        "instructions": instructions,   
-        "corrections": corrections
-    })
-    return {"messages": [response]}
+    def _persona_call(self, state:WorkerState):
+        name = state["name"]
+        prompt = load_prompt(name)
+        question = state["question"]    
+        messages = state["messages"]
+        corrections = state["corrections"]
+        instructions = state["instructions"]
+        chain = prompt | self.llm.bind_tools(tools)
+        logger.info(f"Persona {name} is being executed")
+        response = chain.invoke({
+            "question": question,
+            "messages": messages,
+            "instructions": instructions,   
+            "corrections": corrections
+        })
+        logger.info(f"Persona {name} generated a response")
+        return {"messages": [response]}
 
-def should_use_tools(state:WorkerState):
-    messages = state["messages"]
-    last_message = messages[-1]
-    if last_message.tool_calls:
-        return "tools"
-    return "reflection_call"
+    def _should_use_tools(self, state:WorkerState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        if last_message.tool_calls:
+            return "tools"
+        return "reflection_call"
 
-def reflection_call(state:WorkerState):
-    prompt = load_prompt(name="reflector")
-    chain = prompt | llm
-    question = state["question"]
-    messages = state["messages"]
-    instructions = state["instructions"]
-    last_message = messages[-1]
-    counter = state["counter"]
-    response = chain.invoke({
-        "question": question,
-        "message": last_message,
-        "instructions": instructions
-    })
-    return{ 
-        "counter": counter + 1,
-        "corrections": response.content,
-    }
-
-def should_end(state:WorkerState):
-    counter = state["counter"]
-    if counter < 3:
-        return "persona_call"
-    return "END"
-
-
-def build_persona_graph():
-    graph = StateGraph(WorkerState)
-
-    graph.add_node("persona_call", persona_call)
-    graph.add_node("tools", tool_node)
-    graph.add_node("reflection_call", reflection_call)
-    graph.add_edge(START, "persona_call")
-    graph.add_conditional_edges(
-        source="persona_call",
-        path=should_use_tools,
-        path_map={
-            "tools": "tools",
-            "reflection_call": "reflection_call"
+    def _reflection_call(self, state:WorkerState):
+        prompt = load_prompt(name="reflector")
+        chain = prompt | self.llm
+        question = state["question"]
+        messages = state["messages"]
+        instructions = state["instructions"]
+        last_message = messages[-1]
+        counter = state["counter"]
+        logger.info("Reflector is being called")
+        response = chain.invoke({
+            "question": question,
+            "message": last_message,
+            "instructions": instructions
+        })
+        logger.info("Reflector generated corrections")
+        return{ 
+            "counter": counter + 1,
+            "corrections": response.content,
         }
-    )
-    graph.add_edge("tools", "persona_call")
-    graph.add_conditional_edges(
-        source="reflection_call",
-        path=should_end,
-        path_map={
-            "persona_call": "persona_call",
-            "END": END
-        }
-    )
-    app = graph.compile()
-    return app
 
-def run_persona(persona_graph, persona_initial_state:dict):
-    response = persona_graph.invoke(persona_initial_state)
-    messages = response["messages"]
-    last_message = messages[-1]
-    return last_message
+    def _should_end(self, state:WorkerState):
+        counter = state["counter"]
+        if counter < 2:
+            return "persona_call"
+        return "END"
+
+
+    def _build_persona_graph(self):
+        graph = StateGraph(WorkerState)
+
+        graph.add_node("persona_call", self._persona_call)
+        graph.add_node("tools", ToolNode(tools))
+        graph.add_node("reflection_call", self._reflection_call)
+        graph.add_edge(START, "persona_call")
+        graph.add_conditional_edges(
+            source="persona_call",
+            path=self._should_use_tools,
+            path_map={
+                "tools": "tools",
+                "reflection_call": "reflection_call"
+            }
+        )
+        graph.add_edge("tools", "persona_call")
+        graph.add_conditional_edges(
+            source="reflection_call",
+            path=self._should_end,
+            path_map={
+                "persona_call": "persona_call",
+                "END": END
+            }
+        )
+        app = graph.compile()
+        return app
+
+    def run_worker(self, persona_initial_state:dict):
+        response = self.graph.invoke(persona_initial_state)
+        messages = response["messages"]
+        last_message = messages[-1]
+        return last_message
