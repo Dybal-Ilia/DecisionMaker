0a2e4c5d7831321cf82d3d05074ecc68a98ee957
Dybal-Ilia <dybal.156@gmail.com>
2026-02-12 14:24:02 +0100

    feat: add schemas for arxiv_search tool, add schema for consult_expert tool

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Literal
 from langchain_core.messages import AIMessage
 from pydantic import BaseModel, Field
 import operator
@@ -13,11 +13,21 @@ class ReflectorResponse(BaseModel):
     score: int = Field(default=0, description="Numeric integer response score from 1 to 10")
     corrections: list[str] = Field(default_factory=list, description="A list of corrections that should be applied to agents response")
 
+class ArxiveArticle(BaseModel):
+    title: str = Field(..., description="Title of the article")
+    url: str = Field(..., description="URL associated with the result")
+    summary: str = Field(..., description="A short summary of the article provided by arxiv")
+    authors: list[str] = Field(..., description="A list of authors who contributed to the article")
+
+class ArxivSearchResult(BaseModel):
+    query:str = Field(..., description="Initial search query")
+    articles: list[ArxiveArticle] = Field(..., description="A list of ArxivArticle objects")
+    
 class WorkerState(TypedDict):
     question: str
     name: str
     instructions: str
-    messages: Annotated[list, operator.add]
+    messages: Annotated[list[AIMessage], operator.add]
     counter: int
     score: int
     corrections: str
@@ -27,6 +37,13 @@ class ChatState(TypedDict):
     domain: str
     instructions: list[str]
     final_response: AIMessage
+
+class PersonaCallTool(BaseModel):
+    persona: Literal['Society & Culture', 'Science & Mathematics', 'Health', 'Education & Reference', 'Computers & Internet', 'Sports',
+                     'Business & Finance', 'Entertainment & Music', 'Family & Relationships', 'Politics & Government'] = Field(
+                         ..., description="A name of persona to be called for query execution"
+                     )
+    query: str = Field(..., description="A query that is redirected to another persona (can fully match the initial user query)")
 
 
 
