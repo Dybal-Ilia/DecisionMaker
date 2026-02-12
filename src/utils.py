4f75d30d06aa88d0fb75f96137e679036c281b95
Dybal-Ilia <dybal.156@gmail.com>
2026-02-12 14:27:17 +0100

    feat: change load_prompt util behaviour slightly

--- src/utils.py
+++ src/utils.py
@@ -6,9 +6,7 @@ def load_prompt(name):
     with open(file="src/prompts.yaml", mode="r", encoding="utf-8") as f:
         all_prompts = yaml.safe_load(f)
         prompt_text = all_prompts[name]
-    return ChatPromptTemplate.from_messages([
-        ("system", prompt_text)
-    ])
+    return ChatPromptTemplate.from_template(prompt_text)
 
 def get_logger():
     logger = logging.getLogger("DecisionMaker")
