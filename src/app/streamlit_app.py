1c150dfb5c12de8e32d92e298fa46a7db518234c
Dybal Ilia <dybal.i156@gmail.com>
2026-03-26 15:49:19 +0100

    feat: clean up streamlit_app.py by removing commented-out code and organizing imports

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1,54 +1,13 @@
-import sys
 import asyncio
 from pathlib import Path
-
+import sys
 ROOT = Path(__file__).resolve().parents[2]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
-
-#import torch
 import streamlit as st
 from src.core.workflow import Chat
-
-# from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# @st.cache_data
-# def load_tokenizer_and_model(model_ckpt:str) -> tuple[AutoTokenizer, AutoModelForSequenceClassification]:
-#     """
-#     Loads pretrained tokenizer and model for domain classification
-    
-#     :param model_ckpt: pretrained model name fromm hf
-#     :type model_ckpt: str
-#     """
-
-#     tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
-#     model = AutoModelForSequenceClassification.from_pretrained(model_ckpt).to(device)
-#     return tokenizer, model
-
-# tokenizer, model = load_tokenizer_and_model(model_ckpt="ilia-dybal/domain-classifier")
-
-# def predict_domain(tokenizer, model, query:str):
-#     id2label = {0: 'oos',
-#                 1: 'banking',
-#                 2: 'credit_cards',
-#                 3: 'kitchen_and_dining',
-#                 4: 'home',
-#                 5: 'auto_and_commute',
-#                 6: 'travel',
-#                 7: 'utility',
-#                 8: 'work',
-#                 9: 'small_talk',
-#                 10: 'meta'}
-#     inputs = tokenizer(query, return_tensors="pt").to(device)
-#     with torch.no_grad():
-#         outputs = model(**inputs)
-#         predicted_id = outputs.logits.argmax(-1).item()
-#     label = id2label[predicted_id]
-#     return label
-
-# presets = load_presets()
+from  st_module import login_popup
+from src.core.db import PGManager
 
 domains = ['Society & Culture',
  'Science & Mathematics',
@@ -61,9 +20,26 @@ domains = ['Society & Culture',
  'Family & Relationships',
  'Politics & Government']
 chat = Chat()
-
-
+db = PGManager(
+    dbname="DecisionMaker",
+    host="localhost",
+    user="postgres",
+    password="postgres"
+)
+st.sidebar.title("Navigation Bar")
+st.sidebar.selectbox(
+    label="Navigate to Desired Page",
+    options=["Chat", "My Profile"]
+)    
 st.title("Decision Maker AI Application")
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+
+if not st.session_state.logged_in:
+    login_popup(db=db)
+
 domain = st.selectbox(label="Select a preset", options=domains, placeholder="")
 
 
