ccb8940fdbd5e38abf28d5843eab0d95bf95d1bb
Dybal Ilia <dybal.i156@gmail.com>
2026-04-01 16:46:34 +0200

    feat: refactor PGManager to use async methods for database operations and improve user handling

--- src/core/db.py
+++ src/core/db.py
@@ -1,44 +1,89 @@
 import psycopg
-from dotenv import load_dotenv
 from src.core.schemas import User
-from uuid import uuid4
+import streamlit as st
 import os
-
+import asyncio
+from dotenv import load_dotenv
 load_dotenv()
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DB_URI = os.getenv("DB_URI")
 
 class PGManager:
-    def __init__(self, dbname, user, host, password):
-        try:
-            self.conn = psycopg.connect(
-                dbname=dbname,
-                user=user,
-                host=host,
-                password=password
-            )
-            self.cursor = self.conn.cursor()
-        except:
-            raise psycopg.DatabaseError()
+    def __init__(self, conn: psycopg.AsyncConnection, loop:asyncio.AbstractEventLoop):
+        self.conn = conn
+        self.loop=loop
 
-    def create_user(self, user:User):
-        user_id = user.user_id
-        user_name = user.user_name
-        user_lastname = user.user_lastname
-        user_nickname = user.user_nickname
-        user_password = user.user_password
-        query = f"""INSERT INTO public.users VALUES(
-            '{user_id}',
-            '{user_name}',
-            '{user_lastname}',
-            '{user_nickname}',
-            '{user_password}'
-        )"""
+    async def create_user(self, user: User) -> bool:
+        query = """
+            INSERT INTO public.users (user_id, user_name, user_lastname, user_nickname, user_password)
+            VALUES (%s, %s, %s, %s, %s)
+        """
         try:
-            self.cursor.execute(query)
-            self.conn.commit()
+            async with self.conn.cursor() as cursor:
+                await cursor.execute(query, (
+                    user.user_id,
+                    user.user_name,
+                    user.user_lastname,
+                    user.user_nickname,
+                    user.user_password
+                ))
+            await self.conn.commit()
             return True
         except:
+            await self.conn.rollback() 
             return False
 
+    async def get_user_by_nickname(self, nickname: str):
+        query = """
+            SELECT user_name, user_lastname, user_nickname
+            FROM public.users WHERE user_nickname = %s
+        """
+        try:
+            async with self.conn.cursor() as cursor:
+                await cursor.execute(query, (nickname,))
+                res = await cursor.fetchone()
+                return res
+        except:
+            await self.conn.rollback()
+            return None
         
+    async def get_nicknames_list(self):
+        sql = """
+                SELECT 
+                    user_nickname
+                FROM public.users
+                """
+        try:
+            async with self.conn.cursor() as cursor:
+                await cursor.execute(sql)
+                res = await cursor.fetchall()
+            nickname_list = [r[0] for r in res]
+            return nickname_list
+        except:
+            await self.conn.rollback()
+            return None
+
+    async def fetch_password_by_nickname(self, user_nickname):
+        sql = f"""SELECT 
+                    user_password
+                FROM public.users WHERE user_nickname = %s
+                    """
+        try:
+            async with self.conn.cursor() as cursor:
+                await cursor.execute(sql, (user_nickname,))
+                res = await cursor.fetchone()
+            return res[0]
+        except:
+            await self.conn.rollback()
+            return None
+
+
+@st.cache_resource
+def get_db(_db_uri):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    async def init_conn():
+        return await psycopg.AsyncConnection.connect(_db_uri)
+    conn = loop.run_until_complete(init_conn())
+    manager = PGManager(conn, loop)
+    return manager
