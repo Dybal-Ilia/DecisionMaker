58b40eab93138a22257db287b39572f28441569b
Dybal Ilia <dybal.i156@gmail.com>
2026-04-02 16:49:56 +0200

    feat: add nickname availability check during sign-up process

--- src/app/loginpage.py
+++ src/app/loginpage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_module import check_login, check_password_strong, wrap_user
+from st_module import check_login, check_password_strong, wrap_user, is_nickname_available
 from src.core.db import get_db
 import os
 from dotenv import load_dotenv
@@ -28,6 +28,10 @@ def render_login_popup():
         user_name = st.text_input("Your First Name", key="signup name")
         user_lastname = st.text_input("Your Last Name", key="signup lastname")
         user_nickname = st.text_input("Your Nickname", key="signup nickname")
+        if user_nickname:
+            nickname_available = is_nickname_available(db=db, nickname=user_nickname)
+            if not nickname_available:
+                st.error(f"Nickname '{user_nickname}' is already taken, please choose another one")
         user_password = st.text_input("Your Password", type="password", key="signup password")
         if st.button("Sign Up"):
             is_password_strong = check_password_strong(user_password)
@@ -40,7 +44,7 @@ def render_login_popup():
                     user_nickname=user_nickname,
                     user_password=user_password 
                 )
-                create_user_status = db.loop.run_untill_complete(db.create_user(user))
+                create_user_status = db.loop.run_until_complete(db.create_user(user))
                 if create_user_status:
                     st.session_state.logged_in = True
                     st.session_state.logged_user_nickname = user_nickname
