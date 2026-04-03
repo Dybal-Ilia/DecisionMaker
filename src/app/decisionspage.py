8594269f59f132529e1fecafc13c62ce302a9d4b
Dybal Ilia <dybal.i156@gmail.com>
2026-04-03 16:53:00 +0200

    feat: implement clarifications form and enhance decision page functionality

--- src/app/decisionspage.py
+++ src/app/decisionspage.py
@@ -1,9 +1,36 @@
 import streamlit as st
+from pathlib import Path
+import sys
 
+ROOT = Path(__file__).resolve().parents[2]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+from st_module import get_event_loop
+from st_module import get_drafter_agent
+
+@st.fragment
+def render_clarifications_form(clarifications_list):
+    st.markdown("### Clarifications Needed")
+    st.write("The AI needs a few more details to finalize your draft:")
+
+    with st.form(key="clarifications_form"):
+        temp_responses = {}
+        
+        for i, question in enumerate(clarifications_list):
+            temp_responses[question] = st.text_input(question, key=f"clarification_{i}")
+
+        submitted = st.form_submit_button("Save Answers")
+
+        if submitted:
+            final_qa_dict = {q: a for q, a in temp_responses.items() if a.strip()}
+            st.session_state.draft_clarifications = final_qa_dict
+            
+            st.success("Answers saved successfully!")
 
 def render_decisionpage():
+    loop = get_event_loop()
+    drafter = get_drafter_agent()
     st.header("Make a Decision")
-    st.subheader("Here's how we help you make decisions. Please, fill in all the fields and our AI will help you analyze and choose the best one")
     st.divider()
     with st.container(border=True):
         query = st.text_area(
@@ -11,7 +38,27 @@ def render_decisionpage():
             help="We expect you to describe the problem as detailed as possible. Our AI will make its best to extract your prior probem, possible options and preferences for further decision making."
 
         )
-        
+        if st.button("Generate Draft"):
+            draft = loop.run_until_complete(drafter.generate_draft(query))
+            st.session_state.current_draft = draft
 
-render_decisionpage()
+            st.success("We've prepared a draft for better observability. Make sure to fill up follow-up questions and click 'Make a Decision' if everythin is correct")
+            st.divider()
+            st.markdown(f"### {draft.general_intent} Draft")
 
+            st.markdown("#### Options:")
+                
+            options_text = " &nbsp;&nbsp;|&nbsp;&nbsp; ".join([f"*{opt}*" for opt in draft.options])
+            st.markdown(options_text)
+
+            st.write("")
+
+            st.markdown("### Preferences:")
+            for pref in draft.preferences:
+                st.markdown(f"✓ {pref}")
+
+            st.write("")
+
+            render_clarifications_form(draft.clarifications)
+            st.button("Generate a Decision")
+                
